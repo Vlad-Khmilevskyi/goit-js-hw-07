280ac137a80a3c0bde91508ce413de86e2d8f072
ToFFeeV <khmiluvkiyv.ku@gmail.com>
2023-03-30 17:11:09 +0300

    Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,7 +10,7 @@ function createCardsMarkup(elements) {
     return galleryItems
         .map(({ preview, original, description }) => {
         return `
-        <div class="gallery__item">
+        <li class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img
                 class="gallery__image"
@@ -18,11 +18,13 @@ function createCardsMarkup(elements) {
                 alt="${description}"
                 />
             </a>
-        </div>`;
+        </li>`;
         })
         .join('');
 }
 
 var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+    captionsData: 'alt', captionDelay: 250
 });
+
+// var lightbox =     captionPosition: 'bottom',
